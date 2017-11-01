bd17e3b210db506920173dbacbc06cd8c36d8f41
Céleste Robinson <celestekrobinson@gmail.com>
2017-11-01 13:33:48 -0600

    Main project complete

---
+++ main.js
@@ -0,0 +1,31 @@
+function getFood() {
+    var display = document.getElementById("answer");
+    var foodType = getFoodType();
+    var restaurant = getRandRestaurant(foodType)
+    display.innerText = restaurant;
+}
+
+function getRandRestaurant(foodType)  {
+    if (foodType === 'fast-food') {
+        return fastFood[Math.floor(Math.random()*fastFood.length)];
+    } else if (foodType === 'fast-casual') {
+        return fastCasual[Math.floor(Math.random()*fastCasual.length)];
+    } else if (foodType === 'sit-down') {
+        return sitDown[Math.floor(Math.random()*sitDown.length)];
+    }
+}
+
+function getFoodType() {
+var foodType;
+    if (document.getElementById('fast-food').checked) {
+        foodType = 'fast-food';
+    } else if (document.getElementById('fast-casual').checked) {
+        foodType = 'fast-casual';
+    } else if (document.getElementById('sit-down').checked) {
+        foodType = 'sit-down';
+    } else {
+        alert("Please select a food type so Bae doesn't attack!");
+    }
+    return foodType;
+}
+    
